d13f7ab215b02ae025b9cbbedeccfb851ec5dd49
halloayu <2194877791@qq.com>
2021-12-30 14:17:30 +0800

    权限请求

--- app/src/main/java/com/ayu/camerax/MainActivity.java
+++ app/src/main/java/com/ayu/camerax/MainActivity.java
@@ -1,12 +1,22 @@
 package com.ayu.camerax;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +29,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image);
+
+        checkPermissions();
 
         findViewById(R.id.openCamera).setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
@@ -38,6 +50,26 @@ public class MainActivity extends Activity {
             Glide.with(getApplicationContext())
                     .load(path)
                     .into(imageView);
+        }
+    }
+
+    /*************** 权限申请 ***************/
+
+    private void checkPermissions() {
+        String[] needRequestPermissions = new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+        ActivityCompat.requestPermissions(this, needRequestPermissions, 2);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 2 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "拒绝权限将无法正常使用相机", Toast.LENGTH_LONG).show();
         }
     }
 }
